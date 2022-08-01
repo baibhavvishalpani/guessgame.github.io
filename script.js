87c32c92cd851f9925d76500db18d250b4d05838
BAIBHAV VISHAL PANI <62665504+baibhavvishalpani@users.noreply.github.com>
2022-08-01 20:25:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent ='🎉 Correct Number !';
 
-// document.querySelector('.number').textContent =13;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value=23;
  
 let secretNumber = Math.trunc(Math.random() *20) +1;
 let score =20;
@@ -50,32 +45,13 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('body').style.backgroundColor = '#ff0000';
     }  
   }
-//   else if(guess > secretNumber){
-//     if(score>1){
-//         document.querySelector('.message').textContent='📈 Too high';
-//     score--;
-//     document.querySelector('.score').textContent=score;
-//     }else{
-//         document.querySelector('.message').textContent='💥 You lost the game';
-//         document.querySelector('body').style.backgroundColor = '#ff0000';
-//     }  
-//   }
-//   else if(guess < secretNumber){
-//     if(score>1){
-//         document.querySelector('.message').textContent='📉 Too low';
-//     score--;
-//     document.querySelector('.score').textContent=score;
-//     }else{
-//         document.querySelector('.message').textContent='💥 You lost the game';
-//         document.querySelector('body').style.backgroundColor = '#ff0000';
-//     }  
- // }
+
 });
 document.querySelector('.again').addEventListener('click',function(){
     score =20;
     secretNumber = Math.trunc(Math.random() *20) +1;
 
-    // document.querySelector('.message').textContent='Start guessing...';
+    
     displayMessage('Start guessing...');
     document.querySelector('.score').textContent=score;
     document.querySelector('.number').textContent='?';
